dde3e71360ae76fd3f91ee0f4bd7e5340fc0a69c
Pigamo <58117669+Pigamo@users.noreply.github.com>
2023-06-28 13:03:13 -0400

    no more pp

--- src/components/CardHolder.vue
+++ src/components/CardHolder.vue
@@ -1,117 +1,117 @@
 <script setup lang="ts">
 // get cards
-import { getCardsByDeck, getCards, getDeckList, Card, getWrongCards, getWrongCardsByDeck, updateCard} from '~/firebase/flashcard';
-
+import type { Card } from '~/firebase/flashcard'
+import { getCards, getCardsByDeck, getWrongCards, getWrongCardsByDeck, updateCard } from '~/firebase/flashcard'
 
 // get deck
-const {deck, wrong} = defineProps({
+const { deck, wrong } = defineProps({
   deck: String,
-  wrong: Boolean
+  wrong: Boolean,
 })
 function shuffleArray(array: Card[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]; // swap elements
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]] // swap elements
+  }
+  return array
 }
-const cardList = ref<Card[]>([]);
+const cardList = ref<Card[]>([])
 const pureTotal = ref(0)
-const getAllCardsLocal = async () => {
-  try{
-    if(!deck) return;
-    let cards;
-    if(!wrong){
-        cards = await getCards();
+async function getAllCardsLocal() {
+  try {
+    if (!deck)
+      return
+    let cards
+    if (!wrong)
+      cards = await getCards()
 
-    }
-    else{
-        cards = await getWrongCards();
-    }
-    if(!cards) return;
-    cardList.value = cards;
-    pureTotal.value = cards.length
-    cardList.value = shuffleArray(cardList.value)
-  }
-  catch(err){
-    console.error(err);
-  }
-};
-const getCardsLocal = async () => {
-  try{
-    if(!deck) return;
-    
-    let cards;
-        if (!wrong) {
-            cards = await await getCardsByDeck(deck);
-
-        }
-        else {
-            cards = await getWrongCardsByDeck(deck);
-        }
-    if(!cards) return;
-    cardList.value = cards;
-    pureTotal.value = cards.length
-    cardList.value = shuffleArray(cardList.value)
-  }
-  catch(err){
-    console.error(err);
-  }
-};
-onMounted(()=>{
-    if(deck==='all')
-    getAllCardsLocal()
     else
-    getCardsLocal()
+      cards = await getWrongCards()
+
+    if (!cards)
+      return
+    cardList.value = cards
+    pureTotal.value = cards.length
+    cardList.value = shuffleArray(cardList.value)
+  }
+  catch (err) {
+    console.error(err)
+  }
 }
-    );
+async function getCardsLocal() {
+  try {
+    if (!deck)
+      return
+
+    let cards
+    if (!wrong)
+      cards = await await getCardsByDeck(deck)
+
+    else
+      cards = await getWrongCardsByDeck(deck)
+
+    if (!cards)
+      return
+    cardList.value = cards
+    pureTotal.value = cards.length
+    cardList.value = shuffleArray(cardList.value)
+  }
+  catch (err) {
+    console.error(err)
+  }
+}
+onMounted(() => {
+  if (deck === 'all')
+    getAllCardsLocal()
+  else
+    getCardsLocal()
+},
+)
 const gameOver = ref(false)
-const currentCardIndex = ref(0);
+const currentCardIndex = ref(0)
 const correctTotal = ref(0)
-const markAsComplete = (correct: boolean) => {
-    if(correct) {
-      correctTotal.value++
-      cardList.value[currentCardIndex.value].wrong = false
+function markAsComplete(correct: boolean) {
+  if (correct) {
+    correctTotal.value++
+    cardList.value[currentCardIndex.value].wrong = false
     updateCard(cardList.value[currentCardIndex.value])
-    }
-    else {
-        cardList.value[currentCardIndex.value].wrong = true
-        updateCard(cardList.value[currentCardIndex.value])
-    }
-    if (currentCardIndex.value < cardList.value.length - 1) {
-        currentCardIndex.value++;
-    } else {
-        console.log("All cards have been completed.");
-        gameOver.value = true
-    }
-};
-const toggleLanguage = ref(false)
+  }
+  else {
+    cardList.value[currentCardIndex.value].wrong = true
+    updateCard(cardList.value[currentCardIndex.value])
+  }
+  if (currentCardIndex.value < cardList.value.length - 1) {
+    currentCardIndex.value++
+  }
+  else {
+    // console.log("All cards have been completed.");
+    gameOver.value = true
+  }
+}
 </script>
 
 <template>
-  <div class="card-holder" v-if="deck">
+  <div v-if="deck" class="card-holder">
     <div class="card-holder__header">
-      <h2 class="card-holder__title" v-if="!gameOver && pureTotal>0">{{ currentCardIndex }} / {{ pureTotal }}</h2>
-      <h2 class="card-holder__title" v-if="pureTotal===0">No Cards in Deck!</h2>
+      <h2 v-if="!gameOver && pureTotal > 0" class="card-holder__title">
+        {{ currentCardIndex }} / {{ pureTotal }}
+      </h2>
+      <h2 v-if="pureTotal === 0" class="card-holder__title">
+        No Cards in Deck!
+      </h2>
     </div>
-    
-    
-    <div v-if="gameOver" flex w-full flex-col py10 items-center justify-center text-4xl font-bold space-y-4>
-        <div>
 
-            {{ correctTotal }} / {{ pureTotal }}
-        </div>
-        <div rounded-full h-30 w-30 flex items-center justify-center text-dark :style="`background: rgba(0,255,0,${correctTotal / (pureTotal)})`">
-
-            {{ Math.round((correctTotal / (pureTotal)) * 100) }}%
-        </div> p
-            
+    <div v-if="gameOver" w-full flex flex-col items-center justify-center py10 text-4xl font-bold space-y-4>
+      <div>
+        {{ correctTotal }} / {{ pureTotal }}
+      </div>
+      <div h-30 w-30 flex items-center justify-center rounded-full text-dark :style="`background: rgba(0,255,0,${correctTotal / (pureTotal)})`">
+        {{ Math.round((correctTotal / (pureTotal)) * 100) }}%
+      </div>
     </div>
-    <div class="card-holder__cards" v-if="!gameOver">
-      <FlashCard   :card="cardList[currentCardIndex]" @correct="markAsComplete(true)" @incorrect="markAsComplete(false)" />
-    
+    <div v-if="!gameOver" class="card-holder__cards">
+      <FlashCard :card="cardList[currentCardIndex]" @correct="markAsComplete(true)" @incorrect="markAsComplete(false)" />
     </div>
-   
   </div>
 </template>
 
